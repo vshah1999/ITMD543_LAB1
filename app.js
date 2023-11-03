367d0107dc89a57a613cc164d7fb24877a7e7a9b
vshah1999 <vshah78@hawk.iit.edu>
2023-11-02 20:05:27 -0500

    Install cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+const cors = require("cors")
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -13,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
